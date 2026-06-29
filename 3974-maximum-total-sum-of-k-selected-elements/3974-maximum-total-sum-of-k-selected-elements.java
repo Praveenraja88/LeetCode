24facf56236b80d8fa3568989ea96487748fbde9
Praveenraja88 <praveenraja4493@gmail.com>
2026-06-29 11:38:58 +0530

    Time: 54 ms (50%), Space: 148.9 MB (50%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -10,6 +10,7 @@ class Solution {
 
         int left = 0;
         int right = nums.length - 1;
+        
         while (left < right) {
             int temp = nums[left];
             nums[left] = nums[right];
